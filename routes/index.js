07b3dff76da86e5ecc898617074744e082826149
irfan.manullang <ifmanullang@gmail.com>
2022-10-10 21:15:25 +0700

    fitur transfer

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ const db = require('../models');
 const User = db.users;
 const Op = db.Sequelize.Op;
 
+const Transaksi = db.transaksis;
+const Saldo = db.saldos;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -35,6 +38,84 @@ router.post('/register', function (req, res, next) {
       })
     });
 
+});
+
+//transfer
+router.post('/transfer/:id/:target', function(req, res, next) {
+  var id = parseInt(req.params.id);
+  var target = parseInt(req.params.target);
+  // var idUser = parseFloat(req.body.idUser);
+  // var idTarget = parseFloat(req.body.idTarget);
+  var nominalTransfer = parseFloat(req.body.nominalSaldo);
+
+  // Saldo.findAll({where: {userId: idUser}})
+  Saldo.findByPk(id)
+  .then(saldoLama => {
+    var sisaSaldo = parseFloat(saldoLama.nominalSaldo - nominalTransfer);
+    var nominalSaldo = {
+      nominalSaldo: sisaSaldo
+    }
+
+    //Sudah Mengurangi Saldo Lama 
+    Saldo.update(nominalSaldo, {
+      // where: {userId: idUser}
+      where: {id: id}
+    })
+    .then(saldoBaru => {
+    var transaksi = {
+      idUser: id,
+      idTarget: target,
+      nominalSaldo: nominalTransfer,
+      tanggal: Date(),
+      status: "transfer sukses"
+    }
+      //Membuat History Transaksi
+      Transaksi.create(transaksi)
+      .then(num => {
+
+        // NAMBAH SALDO TARGET
+        // Saldo.findAll({where: {userId: idUser}})
+        Saldo.findByPk(target)
+        .then(saldoLama => {
+          var sisaSaldo = parseFloat(saldoLama.nominalSaldo + nominalTransfer);
+          var nominalSaldo = {
+            nominalSaldo: sisaSaldo
+          }
+
+          //Sudah Mengurangi Saldo Lama 
+          Saldo.update(nominalSaldo, {
+            // where: {userId: idUser}
+            where: {id: target}
+          })
+          .then(saldoBaru => {
+            res.json({
+              saldoSekarang: sisaSaldo,
+              // saldoSekarang: nominalSaldo,
+              status: num.status
+            })
+          })
+          .catch(err => {
+            res.send(err);
+          });
+          
+        })
+        .catch(err => {
+          res.send(err);
+        });
+
+      })
+      .catch(err => {
+          res.send(err);
+      });
+    })
+    .catch(err => {
+      res.send(err);
+    });
+  })
+  .catch(err => {
+    res.send(err);
+  });
+  
 });
 
 module.exports = router;
