9f9f82ba25e3d6c19f9e5b1e02675be538c2def8
wendyhendra90 <wendyhendra90@gmail.com>
2022-10-12 10:41:23 +0700

    transfer_saldo

--- routes/index.js
+++ routes/index.js
@@ -292,13 +292,9 @@ router.post('/transfer_saldo/:id', function(req, res, next) {
               where: {id: idTarget}
             })
             .then(saldoTargetBaru => {//Membuat History Transaksi
-              var transaksi = {
-                idUser: id,
-                idTarget: idTarget,
-                nominalSaldo: saldoSekarang,
-                tanggal: Date(),
-                status: "Debit"
-              }
+              res.json({
+                info: "Transfer Berhasil"
+              })
             })
             .catch(err => {
               res.send(err);
